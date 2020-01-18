ee86493f4dd907abe96338aa7aef16e566bceb0e
nu11secur1ty <venvaropt@gmail.com>
2020-01-18 00:24:54 -0500

    Update ascii.pl

--- Convert::ASCII::Armour/ascii.pl
+++ Convert::ASCII::Armour/ascii.pl
@@ -7,15 +7,15 @@ use Convert::ASCII::Armour;
 my $converter = new Convert::ASCII::Armour; 
  
 my $message   = $converter->armour( 
-                    Object   => "FOO RECORD", 
+                    Object   => "Tarator RECORD", 
                     Headers  => { 
-                                  Table   => "FooBar", 
+                                  Table   => "Tarator", 
                                   Version => "1.23", 
                                 },
                     Content  => { 
-                                  Key  => "0x8738FA7382", 
-                                  Name => "Zoya Hall",
-                                  Pic  => "....",  # gif 
+                                  Key  => "Krastavici", 
+                                  Name => "nu11secur1ty",
+                                  Pic  => ".....",  # gif 
                                 },
                     Compress => 1,
                 );
