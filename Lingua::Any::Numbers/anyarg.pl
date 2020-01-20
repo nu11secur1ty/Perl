aa181e27374778676cdf34c832237a0e8465704a
nu11secur1ty <venvaropt@gmail.com>
2020-01-20 14:09:48 +0200

    Update anyarg.pl

--- Lingua::Any::Numbers/anyarg.pl
+++ Lingua::Any::Numbers/anyarg.pl
@@ -4,19 +4,11 @@ use warnings;
 use JSON;
 use Lingua::Any::Numbers qw(:std);
 #############################################
-# Change Lang
-#my $ln = "PT";
-
+# From 1 to 51 
 my %otr;
 	for (my $i = 1;$i < 51; $i++){
 		$otr {$i} = to_string($i, "@ARGV");	
 	}
-
-# my %ff{'ascii'}=1;
-# print encode_json(\%otr)."\n\n";
-
-my $jsn = JSON->new->ascii(1);
-print $jsn->encode(\%otr)."\n\n";
-       
-#my $jsnn = JSON->new->latin1(0);
-#print $jsnn->encode(\%otr)."\n\n
+# ASCII armor setup 
+	my $jsn = JSON->new->ascii(1);
+		print $jsn->encode(\%otr)."\n\n";
