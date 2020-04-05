21864fdef10e26cd9c23a047045786f234e993fd
nu11secur1ty <venvaropt@gmail.com>
2020-04-05 23:50:36 +0300

    Update p1ng#r.pl

--- p1ng#r/p1ng#r.pl
+++ p1ng#r/p1ng#r.pl
@@ -1,8 +1,9 @@
 #!/usr/bin/perl
+# @nu11secur1ty
 use strict;
 use warnings;
 use diagnostics;
 
 my $site = $ARGV[0];   # give the website
-my $action = `ping $site`;
+  my $action = `ping $site`;
 print "$action\n";
