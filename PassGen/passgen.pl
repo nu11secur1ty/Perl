2c92efb714323d3e387e3a748f9730ba458c009c
nu11secur1ty <venvaropt@gmail.com>
2021-06-25 13:34:14 +0300

    Create passgen.pl

---
+++ PassGen/passgen.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl -W
+
+###########################
+# copyright nu11secur1ty  #
+###########################
+use warnings;
+use strict;
+use Term::ANSIColor;
+
+print color("green"), "Welcome to program 'passgen'!\n",color("reset");
+print color("blue"), "code by nu11secur1ty\n", color("reset");
+print color("red"),"Please press ENTER to generate your password...\n",color ("reset");{
+	<>;
+	}
+my $password;
+   @charlist = (('A'..'Z'), ('a'..'z'), ('!','@','%','^'), (0..9));
+   $range = $#charlist + 1;
+for (1..17) {
+   $password .= $charlist[int(rand($range))];
+}
+   print "Take your password:\n\n$password\n";
