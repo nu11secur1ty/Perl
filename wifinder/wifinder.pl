292b7a749f8be1cfcb033e3c710139f6a3cd600e
nu11secur1ty <venvaropt@gmail.com>
2026-05-03 09:31:08 +0300

    Update wifinder.pl

--- wifinder/wifinder.pl
+++ wifinder/wifinder.pl
@@ -1,49 +1,52 @@
 use strict;
 use warnings;
 use Win32;
-use Win32::Process;
-use Win32::Shell;
 use Encode qw(decode);
 
 # Model name: nu11secur1tyAI
 
 sub is_admin {
-    # Check if the current process has administrative privileges
+    # Returns 1 if admin, 0 otherwise
     return Win32::IsAdminUser();
 }
 
 sub run_as_admin {
     my $script = Win32::GetFullPathName($0);
-    # Relaunch the script with 'runas' verb for UAC elevation
-    Win32::Shell::ShellExecute("runas", "perl", "\"$script\"", "", 1);
+    print "Elevation required. Requesting Administrator rights...\n";
+    
+    # Using PowerShell to restart the script as admin (native to Win 10/11)
+    my $command = "powershell -Command \"Start-Process perl -ArgumentList '$script' -Verb RunAs\"";
+    system($command);
     exit;
 }
 
 sub get_wifi_passwords {
-    # Execute netsh to get profiles
+    # Get all profiles
     my $profiles_raw = `netsh wlan show profiles`;
-    # Windows console usually uses cp1251 in Bulgaria
+    # CP1251 handles Bulgarian/Cyrillic in Windows console
     my $profiles_data = decode('cp1251', $profiles_raw);
     
     my @ssids = $profiles_data =~ /:\s(.*)/g;
     
-    print sprintf("%-3s | %-30s | %-20s\n", "#", "SSID (Network Name)", "Password");
-    print "-" x 60 . "\n";
+    print "\n" . sprintf("%-3s | %-30s | %-20s", "#", "SSID (Network Name)", "Password") . "\n";
+    print "-" x 65 . "\n";
     
     my $count = 1;
     foreach my $name (@ssids) {
-        $name =~ s/\s+$//; # trim trailing whitespace
+        $name =~ s/\s+$//; # Trim trailing whitespace
+        $name =~ s/\r$//;  # Trim carriage return
         next if $name =~ /---/ or $name eq "";
         
-        # Get password for each profile
-        my $info_raw = `netsh wlan show profile name="$name" key=clear`;
+        # Get password for each profile (surround $name with quotes for spaces)
+        my $info_raw = `netsh wlan show profile name=\"$name\" key=clear`;
         my $info = decode('cp1251', $info_raw);
         
-        # Match English or Bulgarian headers
-        my $pass = "No Password";
+        my $pass = "-- Open / Enterprise --";
+        # Match both English "Key Content" and Bulgarian "Съдържание на ключа"
         if ($info =~ /(?:Key Content|Съдържание на ключа)\s*:\s(.*)/) {
             $pass = $1;
             $pass =~ s/\s+$//;
+            $pass =~ s/\r$//;
         }
         
         printf("%-3d | %-30s | %-20s\n", $count++, $name, $pass);
@@ -52,12 +55,12 @@ sub get_wifi_passwords {
 
 sub main {
     if (!is_admin()) {
-        print "Elevation required. Requesting Administrator rights...\n";
         run_as_admin();
     }
 
     print "--- nu11secur1tyAI WiFi Scanner (Perl Version) ---\n";
-    print "System: Windows 11\n\n";
+    print "Working Directory: " . Win32::GetCwd() . "\n";
+    print "System: Windows 11\n";
 
     get_wifi_passwords();
 
